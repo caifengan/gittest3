0bdab26b4a4d4c94fc141cb745696cde0b156451
ancaifeng <ancaifeng123@126.com>
2019-09-20 10:46:07 +0800

    add new file Test1 modify file Test

---
+++ src/com/ancf/demo/Test1.java
@@ -0,0 +1,10 @@
+package com.ancf.demo;
+
+/**
+ * Created by Administrator on 2019/9/20.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(222);
+    }
+}
